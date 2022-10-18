d4c679703e5beb73f0abe232e5e095d4effa247c
Titilayomi1503 <Titilayomi.jt@gmail.com>
2022-10-17 20:44:44 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- * /
+ * main - Print a line code using puts
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
